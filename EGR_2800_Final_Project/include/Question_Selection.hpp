aa461a47c688abf909f328e00e83681e26966f59
AndrewMcGhee <and.mcghee02@gmail.com>
2023-04-19 22:25:24 -0400

    Fixed Questions
    Fixed Questions & Added functions to Question_Selection.hpp

--- EGR_2800_Final_Project/include/Question_Selection.hpp
+++ EGR_2800_Final_Project/include/Question_Selection.hpp
@@ -1,8 +1,10 @@
-#ifndef Question_Selection_H
-#define Question_Selection_H
+#ifndef Question_Selection_H_
+#define Question_Selection_H_
 
 #include <Arduino.h>
 
-
-
+const char* SelectQuestionMath(int Number);
+const char* SelectQuestionGeo(int Number);
+const char* SelectQuestionBio(int Number);
+const char* SelectQuestionTrivia( int Number);
 #endif
--- EGR_2800_Final_Project/src/Question_Selection.cpp
+++ EGR_2800_Final_Project/src/Question_Selection.cpp
@@ -2,9 +2,9 @@
 
 bool Answer; 
 int RandNum;
-/*string*/ int Question;
 
-char SelectQuestionMath(int Number) {
+
+const char* SelectQuestionMath(int Number) {
     if (Number == 1) { 
         Answer = true;
     }
@@ -12,33 +12,44 @@ char SelectQuestionMath(int Number) {
         Answer = false;
     }
     if (Answer == false) {
-        const char* falseMathQuestions[4][128] = { {"(11+9)*12 = 220"},
-                                               {"The absolute value of a real negative number is negative."},
-                                               {"Pi can be correctly written as a fraction."},
-                                               {"-4 is a natural number."}      
-                                             };
-    return *falseMathQuestions [ getRandomValue(1,4) ];
+        const char* falseMathQuestions[3][128] = { "The absolute value of a real negative number is negative.",
+                                                   "Pi can be correctly written as a fraction.",
+                                                   "-4 is a natural number."      
+                                                };
+    return *falseMathQuestions [ random(1,3) ];
     }
     else if (Answer == true) {
-        const char* trueMathQuestions[4][128] = { {"9 x 8 = 72."},
-                                                  {"19 + 14 ÷ 7 = 3 × 7"}
-                                                  {"Natural numbers include all positive counting numbers"}
-                                                  {"Real numbers are the set of all rational and irrational numbers"}
+        const char* trueMathQuestions[3][128] = { "19 + 14 ÷ 7 = 3 × 7",
+                                                  "Natural numbers include all positive counting numbers",
+                                                  "Real numbers are the set of all rational and irrational numbers",
                                                 };
-    return *TrueMathQuestions [ getRandomValue(1,4) ];
+    return *trueMathQuestions [ random(1,3) ];
     }
 
 }
 
-char SelectQuestionGeo(int Number) {
+const char* SelectQuestionGeo(int Number) {
     if (Number == 1) { 
         Answer = true;
     }
     else {
         Answer = false;
     }
-    return(Question); 
-}
+    if (Answer == false) {
+        const char* falseGeoQuestions[3][128] = { "The Great Barrier Reef is located in Australia",
+                                                  "The Mississippi River is the longest river in the United States.",
+                                                  "Mount Rushmore is located in the state of California."
+                                                };
+        return *falseGeoQuestions[ random(1,3) ];
+        }
+    if (Answer == true) {
+        const char* trueGeoQuestions[3][128] = { "Mount Everest is the highest mountain in the world.",
+                                                "The state of Alaska has a coastline longer than the entire contiguous United States.",
+                                                "The highest point in the continental United States is Mount Whitney in California."
+                                              };
+        return *trueGeoQuestions[ random(1,3) ];
+        }
+    }
 
 const char* SelectQuestionBio(int Number) {
     if (Number == 1) { 
@@ -48,32 +59,44 @@ const char* SelectQuestionBio(int Number) {
         Answer = false;
     }
     if (Answer == false) {
-    const char* falseBioQuestions[4][128] = { { "The Large intestine is the longest part of digestive system of human." },
-                                           { "Sharks are mammals." },
-                                           { "The human skeleton is made up of less than 100 bones." },
-                                           { "Water is an example of a chemical element." },
+    const char* falseBioQuestions[3][128] = { "The Large intestine is the longest part of digestive system of human." ,
+                                           "The human skeleton is made up of less than 100 bones.",
+                                           "Water is an example of a chemical element.",
                                          };
 
-    return *falseQuestions[ getRandomValue(1,4) ];
+    return *falseBioQuestions[ random(1,3) ];
     }
     if (Answer == true) {
-    const char* trueBioQuestions[4][128] = { {"Dogs have 42 teeth." }, 
-                                             {"Rabbits are not carnivores."},
-                                             {"Mitochondria is the powerhouse of the cell"},
-                                             {"An algae is a type of autotroph."}
+    const char* trueBioQuestions[3][128] = {"Dogs have 42 teeth.", 
+                                             "Mitochondria is the powerhouse of the cell",
+                                             "An algae is a type of autotroph."
                                            };
 
-    return *trueBioQuestions [ getRandomValue(1,4) ];
-    }
-    
+    return *trueBioQuestions[ random(1,3) ];
+    } 
 }
-char SelectQuestionTrivia( int Number) {
+
+const char* SelectQuestionTrivia( int Number) {
     if (Number == 1) { 
         Answer = true;
     }
     else {
         Answer = false;
     }
-
-    return(Question);
+    if (Answer == false) {
+        const char* falseTriviaQuestions[3][128] {
+            "All the kings in a standard deck of cards have a mustache.",
+            "The name of Batman’s butler is Albert.",
+            "Henry VIII had eight wives."
+        };
+        return *falseTriviaQuestions[random(1,3) ];
+    }
+    if (Answer == true) {
+        const char* trueTriviaQuestions[3][128] {
+            "The tallest mammal in the world is the giraffe.",
+            "George Washington was the first president of the United States.",
+            "Before becoming queen, Queen Elizabeth II trained as a mechanic."
+        };
+        return *trueTriviaQuestions [ random(1,3) ];
+    }
 }
